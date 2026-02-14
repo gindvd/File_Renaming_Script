fdd46c40df2ed1d1ffa5bd74d85a13f23cb62609
gindvd <gingerichdvd@gmail.com>
2026-02-14 14:50:20 +0900

    fix: Minor fixes in the functions.

--- rename_file.py
+++ rename_file.py
@@ -1,4 +1,5 @@
-import os, sys, argparse
+import os, sys
+import argparse
 import math
 
 from PIL import Image
@@ -16,80 +17,83 @@ AUD_EXT = [".m4a", ".mp3", ".wav", ".aac", ".flac", ".ogg", ".wma", ".aiff"]
 def rename_files(args):
   target_directory = args.directory
   set_res_option = args.include_resolution
-  
+
   progress_bar = ProgressBar()
   file_count = 0
   total_files = scan_dir(target_directory)
 
-	for object in os.scandir(target_directory):
+  for object in os.scandir(target_directory):
     # Checks if object is file and not a folder / directory
-		if os.path.isfile(object):
+    if os.path.isfile(object):
       # Separaetes file name and file extension from full path
-			basename = os.path.basename(object)
+      basename = os.path.basename(object)
       # Splits file name from file extenstion
-			filename, extension = os.path.splitext(basename)
+      filename, extension = os.path.splitext(basename)
 
-			filename = replace_with_underscores(filename)
-			filename = remove_punctuation_characters(filename)
-			filename = format_words(filename, extension)
-            
+      filename = replace_with_underscores(filename)
+      filename = remove_punctuation_characters(filename)
+      filename = format_words(filename, extension)
+
       if set_res_option: 
         filename = append_resolution(object, filename, extension)
 
-			new_basename = filename + extension
-			new_filename = os.path.join(target_directory, new_basename)
-			print(new_filename)
-			os.rename(object, new_filename)
-            
-      file_count++
+      new_basename = filename + extension
+      new_filename = os.path.join(target_directory, new_basename)
+      os.rename(object, new_filename)
+
+      file_count += 1
       percentage = math.floor((file_count / total_files) * 100)
-      
+
       progress_bar.update(percentage)
 
 def replace_with_underscores(filename):
-	string = ""
+  string = ""
+  for char in filename:
+    if char == "-" or char == " ":
+      char = "_"
+    
+    string += char
 
-	for char in filename:
-		if char == "-" or char == " ":
-			char = "_"
-		string += char
-
-	return string	
+  return string	
 
 def remove_punctuation_characters(filename):
-	string = ""
+  string = ""
 
-	for char in filename:
-		if char.isalnum() or char == "_":
-			string += char
+  for char in filename:
+    if char.isalnum() or char == "_":
+      string += char
 
-	return string
+  return string
 
 def format_words(filename, extension):
-	media_file_ext = IMG_EXT + VID_EXT + AUD_EXT
-	split_name = filename.split("_")
-	formatted_words = []
+  media_file_ext = IMG_EXT + VID_EXT + AUD_EXT
+  split_name = filename.split("_")
+  formatted_words = []
 
-	for word in split_name:
-		if extension in media_file_ext:
-			word = word.capitalize()
-		else:
-			word = word.lower()
+  for word in split_name:
+    if extension in media_file_ext:
+      word = word.capitalize()
+    else:
+      word = word.lower()
 
-		formatted_words.append(word)
+    formatted_words.append(word)
 
-	return "_".join(formatted_words)
+  return "_".join(formatted_words)
 
 def append_resolution(object, filename, extension):  
   if extension in IMG_EXT:
     with Image.open(object) as image:
       width, height = image.size
-          
-      return (filename += "_" + str(width) + "x" + str(height))
+    
+    appended_name = filename + "_" + str(width) + "x" + str(height)
+
+    return appended_name
+
   # If not an image file, returns file name with no changes
   return filename  
 
 def scan_dir(target_directory):
+  files = 0
   for root, dirnames, filenames in os.walk(target_directory):
     files += len(filenames)
   return files
@@ -97,7 +101,7 @@ def scan_dir(target_directory):
 def parse_args():
   parser = argparse.ArgumentParser(prog='Rename Files Script', 
                                   description='Script renames all files in a given directory.')
-  
+
   parser.add_argument('directory', help='Give the directory where files to be renamed reside.')
   parser.add_argument('-ir','--include-resolution', action='store_true', 
                       help='When set, tells script to include the images resolution in the file name.')
@@ -106,12 +110,12 @@ def parse_args():
 
 def main():
   args = parse_args()
-  
-  if not os.path.isdir(args.directory):
-  print("{} does not exist.".format(args.directory))
-  return
 
-rename_files(args)
+  if not os.path.isdir(args.directory):
+    print("{} does not exist.".format(args.directory))
+    return
+
+  rename_files(args)
 
 if __name__ == "__main__":
   main()    
