1b2d5688a7616d6773087dbcea6c0af7799d71b2
TheOwner <theowner@toaster.thehome.com>
2026-02-14 14:20:49 +0900

    fix: Changed script to use argparse to get optional arguments.

--- rename_file.py
+++ rename_file.py
@@ -1,9 +1,6 @@
-import os
-import sys
-import argparse
+import os, sys, argparse
 import math
 
-from pathlib import Path
 from PIL import Image
 
 from progress_bar import ProgressBar
@@ -16,28 +13,38 @@ VID_EXT = [".mp4", ".mov", ".mkv" , ".wmv", ".avi", "webm", ".mpg", ".mpeg"]
 
 AUD_EXT = [".m4a", ".mp3", ".wav", ".aac", ".flac", ".ogg", ".wma", ".aiff"]
 
-def rename_files(target_directory):
-	total_files = len([name for name in os.listdir(target_directory) if os.path.isfile(os.path.join(target_directory, name))])
-	print(total_files)
-	count = 0
-	progress_bar = ProgressBar()
+def rename_files(args):
+  target_directory = args.directory
+  set_res_option = args.include_resolution
+  
+  progress_bar = ProgressBar()
+  file_count = 0
+  total_files = scan_dir(target_directory)
 
 	for object in os.scandir(target_directory):
+    # Checks if object is file and not a folder / directory
 		if os.path.isfile(object):
+      # Separaetes file name and file extension from full path
 			basename = os.path.basename(object)
+      # Splits file name from file extenstion
 			filename, extension = os.path.splitext(basename)
 
 			filename = replace_with_underscores(filename)
 			filename = remove_punctuation_characters(filename)
 			filename = format_words(filename, extension)
+            
+      if set_res_option: 
+        filename = append_resolution(object, filename, extension)
 
 			new_basename = filename + extension
 			new_filename = os.path.join(target_directory, new_basename)
+			print(new_filename)
 			os.rename(object, new_filename)
-
-			count += 1
-			percentage_value = math.floor((count / total_files) * 100)
-			progress_bar.update(percentage_value)
+            
+      file_count++
+      percentage = math.floor((file_count / total_files) * 100)
+      
+      progress_bar.update(percentage)
 
 def replace_with_underscores(filename):
 	string = ""
@@ -71,16 +78,40 @@ def format_words(filename, extension):
 
 		formatted_words.append(word)
 
-	return "_".join(formatted_words) 
+	return "_".join(formatted_words)
+
+def append_resolution(object, filename, extension):  
+  if extension in IMG_EXT:
+    with Image.open(object) as image:
+      width, height = image.size
+          
+      return (filename += "_" + str(width) + "x" + str(height))
+  # If not an image file, returns file name with no changes
+  return filename  
+
+def scan_dir(target_directory):
+  for root, dirnames, filenames in os.walk(target_directory):
+    files += len(filenames)
+  return files
+
+def parse_args():
+  parser = argparse.ArgumentParser(prog='Rename Files Script', 
+                                  description='Script renames all files in a given directory.')
+  
+  parser.add_argument('directory', help='Give the directory where files to be renamed reside.')
+  parser.add_argument('-ir','--include-resolution', action='store_true', 
+                      help='When set, tells script to include the images resolution in the file name.')
+      
+  return parser.parse_args()
 
 def main():
-	target_directory = sys.argv[1]
+  args = parse_args()
+  
+  if not os.path.isdir(args.directory):
+  print("{} does not exist.".format(args.directory))
+  return
 
-	if not os.path.isdir(target_directory):
-		print("{} does not exist.".format(target_directory))
-		return
-
-	rename_files(target_directory)
+rename_files(args)
 
 if __name__ == "__main__":
   main()    
